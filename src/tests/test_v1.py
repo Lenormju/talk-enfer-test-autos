0e1f281699d474ede682b258c1ec46c09ad83f41
Julien LENORMAND <lenormand@julien0@gmail.com>
2025-10-23 16:36:06 +0200

    wip

--- src/tests/test_v1.py
+++ src/tests/test_v1.py
@@ -2,9 +2,12 @@ from pathlib import Path
 from typing import Iterator
 from textwrap import dedent
 from unittest.mock import Mock
+from typing import Any
 
 import pytest
 import _pytest.monkeypatch
+from testcontainers.postgres import PostgresContainer
+import psycopg2
 
 from app.main_v1 import process_daily_report_v1, reports_dirpath
 
@@ -12,7 +15,8 @@ from app.main_v1 import process_daily_report_v1, reports_dirpath
 def test_process_daily_report_v1(
     fake_daily_report_filepath: Path,
     fake_monthly_report_filepath: Path,
-    mock_database_connection: Mock,
+    # mock_database_connection: Mock,
+    postgres_database_for_test: PostgresContainer,
 ) -> None:
     # Given
     fake_daily_report_filepath.write_text("available_dvds=17")
@@ -43,9 +47,14 @@ def test_process_daily_report_v1(
         10-23: 17
         """
     )
-    mock_database_connection.add_daily_number_of_dvds_available.assert_called_with(
-        "2025-10-23", 17
-    )
+    # mock_database_connection.add_daily_number_of_dvds_available.assert_called_with(
+    #     "2025-10-23", 17
+    # )
+    actual_db_content = list_table_available_dvds(postgres_database_for_test.get_connection_url())
+    assert actual_db_content == [
+        ("toto", "titi",)
+    ]
+
     # TODO: test the MCU movie ?
 
 
@@ -59,16 +68,18 @@ def fake_daily_report_filepath() -> Iterator[Path]:
 
     print(f"{str(daily_report_filepath)!r} created")
     yield daily_report_filepath
-    daily_report_filepath.unlink()
+    daily_report_filepath.unlink(missing_ok=True)
 
 
 @pytest.fixture
 def fake_monthly_report_filepath() -> Iterator[Path]:
     if not reports_dirpath.exists():
         reports_dirpath.mkdir(parents=True)
-    monthly_report_filepath = reports_dirpath / "2025-10_monthly.txt"
-    yield monthly_report_filepath  # TODO(later): make generic
-    monthly_report_filepath.unlink()
+    monthly_report_filepath = (
+        reports_dirpath / "2025-10_monthly.txt"
+    )  # TODO(later): make generic
+    yield monthly_report_filepath
+    monthly_report_filepath.unlink(missing_ok=True)
 
 
 @pytest.fixture
@@ -76,3 +87,31 @@ def mock_database_connection(monkeypatch: _pytest.monkeypatch.MonkeyPatch) -> Mo
     database_conn_mock = Mock()
     monkeypatch.setattr("app.main_v1.DatabaseConnection", lambda: database_conn_mock)
     return database_conn_mock
+
+
+@pytest.fixture
+def postgres_database_for_test(monkeypatch: _pytest.monkeypatch.MonkeyPatch) -> Iterator[PostgresContainer]:
+    db = PostgresContainer(image="docker.io/postgres:latest",
+                           port=5432,
+                           username="postgres",
+                           password="muchsecure",
+                           dbname="postgres",
+                           ).with_bind_ports(5432, 5432)
+    db.start()
+    # setup DB
+    connection = psycopg2.connect(db.get_connection_url().replace("+psycopg2", ""))
+    cursor = connection.cursor()
+    cursor.execute(
+        "create table if not exists daily_number_of_dvds (day VARCHAR(10), available_dvds INTEGER);"
+    )
+    connection.commit()
+
+    yield db
+    db.stop()
+
+
+def list_table_available_dvds(db_connection_url) -> list[tuple[Any, ...]]:
+    connection = psycopg2.connect(db_connection_url)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM daily_number_of_dvds")
+    return cursor.fetchall()
